84a3619fee42caedaee17f21f4d6d1cd2d9f1244
Pavan <pavan.more1@gmail.com>
2022-09-02 20:09:08 +0530

    Initial Commit

---
+++ InstagramFirebaseDemo/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramFirebaseDemo
+//
+//  Created by Pavan More on 02/09/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
---
+++ InstagramFirebaseDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InstagramFirebaseDemo
+//
+//  Created by Pavan More on 02/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+}
+
