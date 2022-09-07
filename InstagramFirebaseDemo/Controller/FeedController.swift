51ac4cabe5fab11f73b2bc6baff0aa104586d13b
Pavan <pavan.more1@gmail.com>
2022-09-07 20:01:38 +0530

    Added Extensions and profileImageView in FeedCell.

---
+++ InstagramFirebaseDemo/Controller/FeedController.swift
@@ -0,0 +1,47 @@
+//
+//  FeedController.swift
+//  InstagramFirebaseDemo
+//
+//  Created by Pavan More on 05/09/22.
+//
+
+import UIKit
+
+private let reuseIdentifier = "cell"
+
+class FeedController: UICollectionViewController {
+    
+    // MARK: - View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        collectionView.backgroundColor = .white
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension FeedController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
+        return cell
+    }
+}
+
+extension FeedController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
