545dd9fe1921a019e186f7940c5b85d31dca704a
Pavan <pavan.more1@gmail.com>
2022-09-07 21:37:10 +0530

    Feedcell UI updated.

--- InstagramFirebaseDemo/View/FeedCell.swift
+++ InstagramFirebaseDemo/View/FeedCell.swift
@@ -17,15 +17,22 @@ class FeedCell: UICollectionViewCell {
         iv.clipsToBounds = true
         iv.isUserInteractionEnabled = true
         iv.image = #imageLiteral(resourceName: "venom-7")
-
         return iv
+    }()
+    
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("Venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return button
     }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
         backgroundColor = .white
         
         addSubview(profileImageView)
@@ -33,9 +40,17 @@ class FeedCell: UICollectionViewCell {
                                 paddingTop:12,  paddingLeft: 12)
         profileImageView.setDimensions(height: 40, width: 40)
         profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView, leftAnchor: profileImageView.rightAnchor, paddingLeft: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Actions
+    @objc func didTapUsername() {
+        print("DEBUG: username tap action")
     }
 }
