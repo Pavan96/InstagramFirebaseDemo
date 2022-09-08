ce9d44a0f3124abca4bc3185a538eba413bb4a8a
Pavan <pavan.more1@gmail.com>
2022-09-08 17:41:19 +0530

    Login screen UI development in progress.

---
+++ InstagramFirebaseDemo/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterationController.swift
+//  InstagramFirebaseDemo
+//
+//  Created by Pavan More on 08/09/22.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
